e6fb10e81557d667a7305050055a7104c39f1b52
SihyeonHong <hongsihyeon179@gmail.com>
2025-11-16 12:29:19 +0900

    tmp: 인지를 못 하네

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Main from "@/components/main";
-import SideBar from "@/components/sidebar";
+import SideBar from "@/components/sid2ebar";
 import Header from "@/components/ui/header";
 
 export default function Page() {
